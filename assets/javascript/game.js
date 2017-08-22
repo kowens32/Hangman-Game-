23d7b334b06927f4d3e19ab3ebf4ae355be8d14a
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-21 20:46:43 -0400

    split update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,21 @@
-var animals {
+
+
+
+//List of possible animals to spell
+var safariAnimals = ["Lion", "Elephant", "Giraffe", "Leopard"];
+
+
+document.onkeyup = function(event) {
+//randomly select an animal
+    var computerAnimal = safariAnimals[Math.floor(Math.random() * safariAnimals.length)];
+
+console.log(computerAnimal);
+
+var lion = "Lion";
+var res = lion.split("",4);
+
+console.log(res);
 
 }
+
+
