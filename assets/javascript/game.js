9cc06f6251e8dd19793346961ed2b27c74b0fe59
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-22 08:42:09 -0400

    initial push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,21 @@
 var safariAnimals = ["Lion", "Elephant", "Giraffe", "Leopard"];
 
 
+
 document.onkeyup = function(event) {
+    var userChoices = event.key;
 //randomly select an animal
     var computerAnimal = safariAnimals[Math.floor(Math.random() * safariAnimals.length)];
 
 console.log(computerAnimal);
 
 var lion = "Lion";
-var res = lion.split("",4);
+var res = lion.split("");
 
 console.log(res);
+
+console.log(userChoices);
+
 
 }
 
