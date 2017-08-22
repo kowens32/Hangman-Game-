ea072f0dfdd0dda29be059085e7ddc4608abc4f0
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-22 14:37:36 -0400

    dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,9 @@
 
 
 //List of possible animals to spell
-var safariAnimals = ["Lion", "Elephant", "Giraffe", "Leopard"];
+var safariAnimals = ["lion", "elephant", "giraffe", "leopard", "rhino", "flamingo", "wildebeest", "buffalo", "hippopotamus", "cheetah",];
 var guessesLeft = 15;
-var correctWord = ("_")*res.length;
+//var correctWord = ("_")*computerAnimalSplit.length;
 
 
 //user selects a key to get started
@@ -12,35 +12,45 @@ document.onkeyup = function(event) {
     var userChoices = event.key;
 //computer randomly selects an animal
     var computerAnimal = safariAnimals[Math.floor(Math.random() * safariAnimals.length)];
+    var computerAnimalSplit = computerAnimal.split("");
+    var correctWord = computerAnimalSplit.length;
+console.log("correct word "+correctWord);
+var dashesAndCorrectGuesses = [];
+    for (i = 0; i < computerAnimalSplit.length; i++) {
+        dashesAndCorrectGuesses.push("_");
+    }
+    console.log("dashes " +dashesAndCorrectGuesses);
+ document.getElementById("currentWord").innerHTML = dashesAndCorrectGuesses.join(" ");
+
+
 
     console.log(computerAnimal);
 
 //Computer has selected a Lion  and splits the string into individual characters
 
-    var lion = "Lion";
-    var res = lion.split("");
-    var validChoices = ("res");
-    console.log(res);
+   // var computerAnimalSplit = computerAnimal.split("");
+    var validChoices = ("computerAnimalSplit");
+    console.log(computerAnimalSplit);
 
     console.log(userChoices);
 
 }
-//If user selects the same str.indexof as the computer than replace with a letter
-    //Else capture letter below
-var lionChoices = ["l", "i", "o", "n"]
-   if (userChoices !== -1) and (userChoices < res.length)
-        userChoices[res]=event.key;
+//If user selects the same str.indexof as the computer than replace with the letter
+
+// var lionChoices = ["l", "i", "o", "n"]
+//    if (userChoices !== -1) and (userChoices < computerAnimalSplit.length)
+//       userChoices[computerAnimalSplit]=event.key;
 
 
-//If user selects a choice that does not equal res.length than detract from guessesLeft
-if (validChoices.indexOf(userChoices) > res.length) {
-    guessesLeft--;
-//then capture below
-    var incorrectUserChoice = document.getElementById("incorrect");
-    incorrectUserChoice.innerHTML = userChoices;
-}
-
-
+// //If user selects a choice that does not equal computerAnimalSplit.length than subtract from guessesLeft
+// if (validChoices.indexOf(userChoices) > computerAnimalSplit.length) {
+//     guessesLeft--;
+// //then capture below
+//     var incorrectUserChoice = document.getElementById("incorrect");
+//     incorrectUserChoice.innerHTML = userChoices;
+// }
+//
+//
 
 
 
