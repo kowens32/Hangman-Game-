a9c4e231fcb108816d6a5869885dd324bc2f5a97
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-22 15:51:32 -0400

    correct letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,46 +6,85 @@ var safariAnimals = ["lion", "elephant", "giraffe", "leopard", "rhino", "flaming
 var guessesLeft = 15;
 //var correctWord = ("_")*computerAnimalSplit.length;
 
+var computerAnimal = safariAnimals[Math.floor(Math.random() * safariAnimals.length)];
+var computerAnimalSplit = computerAnimal.split("");
+console.log(computerAnimal);
+var userChoices='';
+var correctLetter = false;
 
 //user selects a key to get started
 document.onkeyup = function(event) {
     var userChoices = event.key;
 //computer randomly selects an animal
-    var computerAnimal = safariAnimals[Math.floor(Math.random() * safariAnimals.length)];
-    var computerAnimalSplit = computerAnimal.split("");
+//     var computerAnimal = safariAnimals[Math.floor(Math.random() * safariAnimals.length)];
+   // var computerAnimalSplit = computerAnimal.split("");
     var correctWord = computerAnimalSplit.length;
-console.log("correct word "+correctWord);
-var dashesAndCorrectGuesses = [];
+    console.log("correct word "+correctWord);
+    var dashesAndCorrectGuesses = [];
     for (i = 0; i < computerAnimalSplit.length; i++) {
         dashesAndCorrectGuesses.push("_");
     }
     console.log("dashes " +dashesAndCorrectGuesses);
- document.getElementById("currentWord").innerHTML = dashesAndCorrectGuesses.join(" ");
+    document.getElementById("currentWord").innerHTML = dashesAndCorrectGuesses.join(" ");
 
 
 
     console.log(computerAnimal);
-
+  //  console.log(correctLetter);
 //Computer has selected a Lion  and splits the string into individual characters
 
-   // var computerAnimalSplit = computerAnimal.split("");
+    // var computerAnimalSplit = computerAnimal.split("");
     var validChoices = ("computerAnimalSplit");
-    console.log(computerAnimalSplit);
+    console.log("event function " +computerAnimalSplit);
 
-    console.log(userChoices);
+    console.log("event function user choice " +userChoices);
+    var correctLetter = false;
+    for (i=0; i < computerAnimalSplit.length; i++) {
+        if (computerAnimalSplit[i] === userChoices) {
+            correctLetter = true;
+            console.log("for loop " + userChoices);
+            console.log("for loop split " + computerAnimalSplit[i])
+            //console.log("for loop correct " + correctLetter);
+        }
+        console.log("for loop correct " + correctLetter);
+    }
+
 
 }
-//If user selects the same str.indexof as the computer than replace with the letter
 
-// var lionChoices = ["l", "i", "o", "n"]
-//    if (userChoices !== -1) and (userChoices < computerAnimalSplit.length)
-//       userChoices[computerAnimalSplit]=event.key;
+
+
+
+
+
+
+
+
+
+
+//If user selects letter that matches a letter in the array, replace dash with correct letter in the correct position
+
+//var correctLetter = false;
+
+// for (i=0; i < computerAnimalSplit.length; i++) {
+//   //  console.log("we are in the last for loop ");
+//     console.log(userChoices);
+//     console.log("split "+computerAnimalSplit[i]);
+//     if (computerAnimalSplit[i] === userChoices) {
+//         console.log(userChoices);
+//         console.log("split "+computerAnimalSplit[i])
+//         correctLetter = true;
+//     }
+// }
+
+
+//console.log(correctLetter);
 
 
 // //If user selects a choice that does not equal computerAnimalSplit.length than subtract from guessesLeft
 // if (validChoices.indexOf(userChoices) > computerAnimalSplit.length) {
 //     guessesLeft--;
-// //then capture below
+// //then capture the letter below
 //     var incorrectUserChoice = document.getElementById("incorrect");
 //     incorrectUserChoice.innerHTML = userChoices;
 // }
